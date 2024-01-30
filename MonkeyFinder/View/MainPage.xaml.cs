8fda90fbb24521a56c2e18f82fb7e2cc05e4e753
Ayaz Ahmed Memon <ayazahmed313@gmail.com>
2024-01-31 03:01:44 +0500

    using mvvm, dependency injection, rest api
    1. creating a service to get monkeys data from api
    2. MVVM pattern to bind data/command to MainPage
    3. Register dependency services on MauiProgram.cs

--- MonkeyFinder/View/MainPage.xaml.cs
+++ MonkeyFinder/View/MainPage.xaml.cs
@@ -2,9 +2,9 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage(MonkeysViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
 }
-
